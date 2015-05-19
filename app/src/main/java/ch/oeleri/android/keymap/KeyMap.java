93cb34ddbb9ae6381bcddcdd82c7f76ba5ad032d
ChunkLightTuna <chris@oeleri.ch>
2015-05-19 14:23:05 -0400

    Initial commit

---
+++ app/src/main/java/ch/oeleri/android/keymap/KeyMap.java
@@ -0,0 +1,44 @@
+package ch.oeleri.android.keymap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.TextView;
+
+
+public class KeyMap extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+    }
+
+    TextView text;
+    int backCount;
+
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent event)
+    {
+        String keyPressInfo =
+                "KeyCode: " + keycode +
+                " / " + KeyEvent.keyCodeToString(keycode) +
+                "\n deviceID: " + event.getDeviceId();
+
+        text.setText(keyPressInfo);
+
+        Log.d("keyPress", keyPressInfo);
+
+        if( keycode == 4 )
+            backCount++;
+        else
+            backCount = 0;
+
+        if( backCount == 5 )
+            finish();
+
+        return true;
+    }
+}
